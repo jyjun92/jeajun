3e23b945264e493b6af729707af9fda3e2423a91
Jea Yoen Jun <jea.jun422@gmail.com>
2020-05-03 15:16:37 -0500

    Updates

---
+++ precache-manifest.c70ca57de5a940e082060d435be52fa5.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a973deb1cdf4c7facc25ac426126b6",
+    "url": "/jeajun/index.html"
+  },
+  {
+    "revision": "63dd259fafe7e31cc088",
+    "url": "/jeajun/static/js/2.a06172c2.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/jeajun/static/js/2.a06172c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c780e22a01ab0e7857",
+    "url": "/jeajun/static/js/main.7900f519.chunk.js"
+  },
+  {
+    "revision": "ed7efdef025b1b8101a4",
+    "url": "/jeajun/static/js/runtime-main.3e391756.js"
+  },
+  {
+    "revision": "154bf4842835b23fd3c191319ef3f628",
+    "url": "/jeajun/static/media/beautyproducts.154bf484.png"
+  },
+  {
+    "revision": "a7645ef61a409caa6a42ac8634ff8691",
+    "url": "/jeajun/static/media/cover.a7645ef6.png"
+  },
+  {
+    "revision": "3e29014a31839ab84aa3f6e8289eeaef",
+    "url": "/jeajun/static/media/coverimg.3e29014a.png"
+  },
+  {
+    "revision": "56b4ac1551b298a8af2a60b3a08e5dce",
+    "url": "/jeajun/static/media/filedownloaded.56b4ac15.png"
+  },
+  {
+    "revision": "44b3341cd25577ff7641a4d5daa8ebd1",
+    "url": "/jeajun/static/media/findjob.44b3341c.jpg"
+  },
+  {
+    "revision": "cda1dc4d1ddbb44673fa44ad84081811",
+    "url": "/jeajun/static/media/gorumaingo.cda1dc4d.png"
+  },
+  {
+    "revision": "f526741de491d1f2bea743c3bedb4ee9",
+    "url": "/jeajun/static/media/image1.f526741d.png"
+  },
+  {
+    "revision": "48e2aa83f1420bfeee8c17ac0277edf3",
+    "url": "/jeajun/static/media/image2.48e2aa83.png"
+  },
+  {
+    "revision": "37b19c52d4f4d1ae114fb5573ab9367f",
+    "url": "/jeajun/static/media/image3.37b19c52.png"
+  },
+  {
+    "revision": "36f4c8ffc5445906c6d14255a30f1dbd",
+    "url": "/jeajun/static/media/indeedjob.36f4c8ff.png"
+  },
+  {
+    "revision": "8c7d77c60974ed9886c3fa227eabdaad",
+    "url": "/jeajun/static/media/initial.8c7d77c6.png"
+  },
+  {
+    "revision": "9a0cb269bfc6299ca8702f58805c45ed",
+    "url": "/jeajun/static/media/katalk1.9a0cb269.jpg"
+  },
+  {
+    "revision": "1a981c7d68ba2e9234213f0e7533ba04",
+    "url": "/jeajun/static/media/movie_detail.1a981c7d.png"
+  },
+  {
+    "revision": "e3be357e6a278357763b8c9853483666",
+    "url": "/jeajun/static/media/movie_upcoming.e3be357e.png"
+  },
+  {
+    "revision": "c87e91a7eef4395cacfc15c0fe37ca8f",
+    "url": "/jeajun/static/media/movieapp.c87e91a7.png"
+  },
+  {
+    "revision": "fbf0f79d541d9a5463ca58c69759b330",
+    "url": "/jeajun/static/media/mykatalk1.fbf0f79d.png"
+  },
+  {
+    "revision": "d4d5986b2826b8db8b47b8907f6b9ada",
+    "url": "/jeajun/static/media/mykatalk2.d4d5986b.png"
+  },
+  {
+    "revision": "3d033f35c394b66eed41c4af20dc638a",
+    "url": "/jeajun/static/media/mykatalk3.3d033f35.png"
+  },
+  {
+    "revision": "83fd8c77b4e51f6f325e659570b4fa06",
+    "url": "/jeajun/static/media/mykatalk4.83fd8c77.png"
+  },
+  {
+    "revision": "fa43fd3a50201be607e639a21e561e4d",
+    "url": "/jeajun/static/media/mykatalk5.fa43fd3a.png"
+  },
+  {
+    "revision": "13dee6a133cebb9c53ac3c166dfa9ef1",
+    "url": "/jeajun/static/media/outcome.13dee6a1.png"
+  },
+  {
+    "revision": "bb8844d6c60d1e8baec3f9b7e2cbd30e",
+    "url": "/jeajun/static/media/output.bb8844d6.png"
+  },
+  {
+    "revision": "2484954b108508fb1b2a9fa55dcb9937",
+    "url": "/jeajun/static/media/paintcover.2484954b.png"
+  },
+  {
+    "revision": "df9452a61f6aece1293e1c9e84dfeb93",
+    "url": "/jeajun/static/media/painting.df9452a6.jpg"
+  },
+  {
+    "revision": "582b0c5a8a74ee9b889fd54b8cde6817",
+    "url": "/jeajun/static/media/search_initial.582b0c5a.png"
+  },
+  {
+    "revision": "d9c4f622ca768d475eb50e81bf796011",
+    "url": "/jeajun/static/media/search_keywordentered.d9c4f622.png"
+  },
+  {
+    "revision": "7ba0ca0e0408e488acfaece74f75b24a",
+    "url": "/jeajun/static/media/sephoraingredient.7ba0ca0e.png"
+  },
+  {
+    "revision": "0b4276f3e7803f22122d246c2d39322c",
+    "url": "/jeajun/static/media/sephoraproductpage.0b4276f3.png"
+  },
+  {
+    "revision": "9122df9c923c48a4e8c5b17ca7f8a090",
+    "url": "/jeajun/static/media/tv_airingtoday.9122df9c.png"
+  },
+  {
+    "revision": "9fc95fbdd54d6323dce25ab9a0dfe5ef",
+    "url": "/jeajun/static/media/tv_toprated.9fc95fbd.png"
+  }
+]);
